3c113d26adfb6304e28338f7c2426f8f7f7f9f06
Stephane Sob Fouodji <stefan-kruger7@hotmail.com>
2024-06-10 23:02:43 +0200

    confirm modal added: missing config TODo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { HttpClientModule } from '@angular/common/http';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, RouterLink, HttpClientModule],
+  imports: [RouterOutlet, MatButtonModule, RouterLink, HttpClientModule, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
